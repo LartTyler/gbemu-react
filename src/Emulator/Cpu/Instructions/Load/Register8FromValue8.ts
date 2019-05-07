744421abeaa734563c1296f6d7ec1bc5b3ae7a9b
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-05-07 18:02:21 -0400

    Add LD r8, n8 instructions

---
+++ src/Emulator/Cpu/Instructions/Load/Register8FromValue8.ts
@@ -0,0 +1,16 @@
+import {IHardwareBus} from '../../../Hardware';
+import {CpuRegister} from '../../Cpu';
+import {Instruction} from '../../Instruction';
+
+/**
+ * LD r8, n8
+ */
+export class Register8FromValue8 extends Instruction {
+	public constructor(code: number, protected target: CpuRegister) {
+		super(code, `LD ${target.toUpperCase()}, (PC)`, 2, 2);
+	}
+
+	protected invoke(hardware: IHardwareBus): void {
+		hardware.cpu.registers[this.target] = hardware.memory.read(hardware.cpu.registers.programCounter);
+	}
+}
--- src/Emulator/Cpu/Instructions/Load/index.ts
+++ src/Emulator/Cpu/Instructions/Load/index.ts
@@ -1,7 +1,17 @@
 import {Register16AddressFromRegister8} from './Register16AddressFromRegister8';
 import {Register16FromValue16} from './Register16FromValue16';
+import {Register8FromValue8} from './Register8FromValue8';
 
 export const loadInstructions = [
+	// LD r8, n8
+	new Register8FromValue8(0x06, 'b'),
+	new Register8FromValue8(0x0E, 'c'),
+	new Register8FromValue8(0x16, 'd'),
+	new Register8FromValue8(0x1E, 'e'),
+	new Register8FromValue8(0x26, 'h'),
+	new Register8FromValue8(0x2E, 'l'),
+	new Register8FromValue8(0x3E, 'a'),
+
 	// LD r16, n16
 	new Register16FromValue16(0x01, 'b', 'c'),
 	new Register16FromValue16(0x11, 'd', 'e'),
