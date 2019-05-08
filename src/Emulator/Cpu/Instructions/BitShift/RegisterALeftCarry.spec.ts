0cdbc9acb7206fd77ef3d34baa72ffa95ecfa483
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-05-07 21:29:28 -0400

    Add RLA instruction

---
+++ src/Emulator/Cpu/Instructions/BitShift/RegisterALeftCarry.spec.ts
@@ -0,0 +1,34 @@
+import {HardwareBus} from '../../../Hardware/HardwareBus';
+import {RegisterFlag} from '../../Cpu';
+import {instructions} from '../index';
+
+describe('RLA', () => {
+	const hardware = new HardwareBus();
+	const registers = hardware.cpu.registers;
+
+	test('RLA', () => {
+		const instruction = instructions.get(0x17);
+
+		registers.a = 0b0001;
+
+		instruction.execute(hardware);
+
+		expect(registers.a).toBe(0b0010);
+		expect(registers.flags).toBe(0);
+
+		expect(hardware.cpu.clock).toBe(1);
+		expect(registers.programCounter).toBe(1);
+
+		registers.a = 0b10000000;
+
+		instruction.execute(hardware);
+
+		expect(registers.a).toBe(0);
+		expect(registers.flags).toBe(RegisterFlag.CARRY);
+
+		instruction.execute(hardware);
+
+		expect(registers.a).toBe(0b0001);
+		expect(registers.flags).toBe(0);
+	});
+});
--- src/Emulator/Cpu/Instructions/BitShift/index.ts
+++ src/Emulator/Cpu/Instructions/BitShift/index.ts
@@ -1,6 +1,10 @@
 import {RegisterALeft} from './RegisterALeft';
+import {RegisterALeftCarry} from './RegisterALeftCarry';
 
 export const bitshiftInstructions = [
 	// RLCA
 	new RegisterALeft(0x07),
+
+	// RLA
+	new RegisterALeftCarry(0x17),
 ];
