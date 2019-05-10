7397bad557c9c220c9babfa322ffad623aa1394f
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-05-10 09:29:23 -0400

    Update CPU clock based on outcome of jump condition

--- src/Emulator/Cpu/Instructions/Jump/ConditionalRelativeSigned8.ts
+++ src/Emulator/Cpu/Instructions/Jump/ConditionalRelativeSigned8.ts
@@ -13,6 +13,8 @@ export class ConditionalRelativeSigned8 extends Instruction {
 	}
 
 	protected invoke(hardware: IHardwareBus): void {
+		hardware.cpu.clock += 2;
+
 		const registers = hardware.cpu.registers;
 		const flagSet = registers.flags & this.flag;
 
@@ -21,7 +23,10 @@ export class ConditionalRelativeSigned8 extends Instruction {
 		// Only jump if:
 		//     - The flag is set and this instruction requires the flag to be set
 		//     - The flag is NOT set, and this instructions requires the flag NOT to be set
-		if (this.requireSet && flagSet || !this.requireSet && flagSet === 0)
+		if (this.requireSet && flagSet || !this.requireSet && flagSet === 0) {
 			registers.programCounter += jump;
+
+			hardware.cpu.clock += 1;
+		}
 	}
 }
