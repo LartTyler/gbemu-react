4b0c9175207478c5fb235ef8fc1b5945ce20b0c2
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-05-11 15:11:20 -0400

    Update ADC r8, r8 to use common code

--- src/Emulator/Cpu/Instructions/Add/Register8ToRegister8Carry.ts
+++ src/Emulator/Cpu/Instructions/Add/Register8ToRegister8Carry.ts
@@ -1,11 +1,11 @@
 import {IHardwareBus} from '../../../Hardware';
-import {Instruction} from '../../Instruction';
-import {CpuRegister8, RegisterFlag} from '../../Registers';
+import {CpuRegister8} from '../../Registers';
+import {AbstractToRegister8Carry} from './AbstractToRegister8Carry';
 
 /**
  * ADC r8, r8
  */
-export class Register8ToRegister8Carry extends Instruction {
+export class Register8ToRegister8Carry extends AbstractToRegister8Carry {
 	public constructor(code: number, protected target: CpuRegister8, protected source: CpuRegister8) {
 		super(code, `ADC ${target.toUpperCase()}, ${source.toUpperCase()}`, 1, 1);
 	}
@@ -13,23 +13,6 @@ export class Register8ToRegister8Carry extends Instruction {
 	protected invoke(hardware: IHardwareBus): void {
 		const registers = hardware.cpu.registers;
 
-		const a = registers[this.target];
-		const b = registers[this.source];
-		const carry = registers.flags & RegisterFlag.CARRY;
-
-		registers.flags = 0;
-
-		if ((a & 0xF) + (b & 0xF) + 1 & 0x10)
-			registers.flags |= RegisterFlag.HALF_CARRY;
-
-		const result = a + b + (carry ? 1 : 0);
-
-		if (result > 0xFF)
-			registers.flags |= RegisterFlag.CARRY;
-
-		registers[this.target] = result;
-
-		if (registers[this.target] === 0)
-			registers.flags |= RegisterFlag.ZERO;
+		registers[this.target] = this.process(registers, registers[this.target], registers[this.source]);
 	}
 }
