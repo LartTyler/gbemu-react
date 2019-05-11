b373d95dcdbd66c45a9576b98c342a9fca8e9e2a
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-05-11 01:56:06 -0400

    Add NOP test

---
+++ src/Emulator/Cpu/Instructions/Misc/Noop.spec.ts
@@ -0,0 +1,13 @@
+import {HardwareBus} from '../../../Hardware/HardwareBus';
+import {instructions} from '../index';
+
+describe('NOP', () => {
+	const hardware = new HardwareBus();
+
+	test('NOP', () => {
+		instructions.get(0x00).execute(hardware);
+
+		expect(hardware.cpu.registers.programCounter).toBe(1);
+		expect(hardware.cpu.clock).toBe(1);
+	});
+});
